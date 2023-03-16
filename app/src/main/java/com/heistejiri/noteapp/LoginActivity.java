40df0491b0248abfb4542d8912cfa77c46e7c1f7
Oghenetejiri Agoro <heistejiri@gmail.com>
2023-03-16 15:53:56 +0100

    Update LoginActivity.java
    Finished Everything About Login Page

--- app/src/main/java/com/heistejiri/noteapp/LoginActivity.java
+++ app/src/main/java/com/heistejiri/noteapp/LoginActivity.java
@@ -20,12 +20,106 @@ import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private EditText mloginemail,mloginpassword;
+    private RelativeLayout mlogin,mgotosignup;
+    private  TextView mgotoforgotpassword;
+
     private FirebaseAuth firebaseAuth;
+
+    ProgressBar mprogressbarofmainactivity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         getSupportActionBar().hide();
+
+        getSupportActionBar().hide();
+
+        mloginemail=findViewById(R.id.loginemail);
+        mloginpassword=findViewById(R.id.loginpassword);
+        mlogin=findViewById(R.id.login);
+        mgotoforgotpassword=findViewById(R.id.gotoforgotpassword);
+        mgotosignup=findViewById(R.id.gotosignup);
+        mprogressbarofmainactivity=findViewById(R.id.progressbarofmainactivity);
+
+        firebaseAuth=FirebaseAuth.getInstance();
+        FirebaseUser firebaseUser=firebaseAuth.getCurrentUser();
+
+        if(firebaseUser!=null)
+        {
+            finish();
+            startActivity(new Intent(LoginActivity.this,MainActivity.class));
+        }
+
+        mgotosignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this,SignupActivity.class));
+            }
+        });
+
+        mgotoforgotpassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this,forgotpassword.class));
+            }
+        });
+
+
+        mlogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String mail=mloginemail.getText().toString().trim();
+                String password=mloginpassword.getText().toString().trim();
+
+                if(mail.isEmpty()|| password.isEmpty())
+                {
+                    Toast.makeText(getApplicationContext(),"All Field Are Required",Toast.LENGTH_SHORT).show();
+
+                }
+                else
+                {
+                    // login the user
+                    mprogressbarofmainactivity.setVisibility(View.VISIBLE);
+
+                    firebaseAuth.signInWithEmailAndPassword(mail,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+
+
+                            if(task.isSuccessful())
+                            {
+                                checkmailverfication();
+                            }
+                            else
+                            {
+                                Toast.makeText(getApplicationContext(),"Account Doesn't Exist",Toast.LENGTH_SHORT).show();
+                                mprogressbarofmainactivity.setVisibility(View.INVISIBLE);
+                            }
+
+
+                        }
+                    });
+                }
+            }
+        });
+    }
+    private void checkmailverfication()
+    {
+        FirebaseUser firebaseUser=firebaseAuth.getCurrentUser();
+
+        if(firebaseUser.isEmailVerified()==true)
+        {
+            Toast.makeText(getApplicationContext(),"Logged In",Toast.LENGTH_SHORT).show();
+            finish();
+            startActivity(new Intent(LoginActivity.this,MainActivity.class));
+        }
+        else
+        {
+            mprogressbarofmainactivity.setVisibility(View.INVISIBLE);
+            Toast.makeText(getApplicationContext(),"Verify your mail first",Toast.LENGTH_SHORT).show();
+            firebaseAuth.signOut();
+        }
     }
 }
