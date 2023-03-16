7a26510c29b466894cc12ae60298845096e1b915
Oghenetejiri Agoro <heistejiri@gmail.com>
2023-03-16 15:49:40 +0100

    debugging
    Trying to resolve firebase auth

--- app/src/main/java/com/heistejiri/noteapp/LoginActivity.java
+++ app/src/main/java/com/heistejiri/noteapp/LoginActivity.java
@@ -12,7 +12,15 @@ import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class LoginActivity extends AppCompatActivity {
+
+    private FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
