62b6d70adb110bd2c08b389f480058dfeefde005
Oghenetejiri Agoro <heistejiri@gmail.com>
2023-03-16 14:49:34 +0100

    Updated Splash Activity

--- app/src/main/java/com/heistejiri/noteapp/SplashActivity.java
+++ app/src/main/java/com/heistejiri/noteapp/SplashActivity.java
@@ -2,13 +2,28 @@ package com.heistejiri.noteapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
 
 public class SplashActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent=new Intent(SplashActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },4000);
+
     }
 }
