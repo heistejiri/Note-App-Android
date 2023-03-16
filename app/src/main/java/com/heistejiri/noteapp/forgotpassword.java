c0aeaba0491fd22894d1e8a462a0238559a63f6b
Oghenetejiri Agoro <heistejiri@gmail.com>
2023-03-16 17:03:43 +0100

    Update forgotpassword.java

--- app/src/main/java/com/heistejiri/noteapp/forgotpassword.java
+++ app/src/main/java/com/heistejiri/noteapp/forgotpassword.java
@@ -1,10 +1,16 @@
 package com.heistejiri.noteapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 public class forgotpassword extends AppCompatActivity {
+
+    private EditText mforgotpassword;
+    private Button mpasswordrecoverbutton;
+    private TextView mgobacktologin;
+
+    FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
