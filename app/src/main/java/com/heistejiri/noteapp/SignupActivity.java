7574b2a6836327b56d14c9ccce9c21c19dc9f4ff
Oghenetejiri Agoro <heistejiri@gmail.com>
2023-03-16 15:59:47 +0100

    Update SignupActivity.java
    Completed everything is SignUp.Java

--- app/src/main/java/com/heistejiri/noteapp/SignupActivity.java
+++ app/src/main/java/com/heistejiri/noteapp/SignupActivity.java
@@ -19,9 +19,99 @@ import com.google.firebase.auth.FirebaseUser;
 
 public class SignupActivity extends AppCompatActivity {
 
+    private EditText msignupemail,msignuppassword;
+    private RelativeLayout msignup;
+    private TextView mgotologin;
+
+    private FirebaseAuth firebaseAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        getSupportActionBar().hide();
+
+        msignupemail=findViewById(R.id.signupemail);
+        msignuppassword=findViewById(R.id.signuppassword);
+        msignup=findViewById(R.id.signup);
+        mgotologin=findViewById(R.id.gotologin);
+
+        firebaseAuth= FirebaseAuth.getInstance();
+
+        mgotologin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(SignupActivity.this,LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        msignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String mail=msignupemail.getText().toString().trim();
+                String password=msignuppassword.getText().toString().trim();
+
+                if(mail.isEmpty() || password.isEmpty())
+                {
+                    Toast.makeText(getApplicationContext(),"All Fields are Required",Toast.LENGTH_SHORT).show();
+                }
+                else if(password.length()<7)
+                {
+                    Toast.makeText(getApplicationContext(),"Password Should Greater than 7 Digits",Toast.LENGTH_SHORT).show();
+                }
+                else
+                {
+                    /// registered the user to firebase
+
+                    firebaseAuth.createUserWithEmailAndPassword(mail,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+
+                            if(task.isSuccessful())
+                            {
+                                Toast.makeText(getApplicationContext(),"Registration Successful",Toast.LENGTH_SHORT).show();
+                                sendEmailVerification();
+                            }
+                            else
+                            {
+                                Toast.makeText(getApplicationContext(),"Failed To Register",Toast.LENGTH_SHORT).show();
+                            }
+
+
+                        }
+                    });
+
+                }
+
+            }
+        });
+
     }
+
+    //send email verification
+    private void sendEmailVerification()
+    {
+        FirebaseUser firebaseUser=firebaseAuth.getCurrentUser();
+        if(firebaseUser!=null)
+        {
+            firebaseUser.sendEmailVerification().addOnCompleteListener(new OnCompleteListener<Void>() {
+                @Override
+                public void onComplete(@NonNull Task<Void> task) {
+                    Toast.makeText(getApplicationContext(),"Verification Email is Sent,Verify and Log In Again",Toast.LENGTH_SHORT).show();
+                    firebaseAuth.signOut();
+                    finish();
+                    startActivity(new Intent(SignupActivity.this,MainActivity.class));
+                }
+            });
+        }
+
+        else
+        {
+            Toast.makeText(getApplicationContext(),"Failed To Send Verification Email",Toast.LENGTH_SHORT).show();
+        }
+    }
+
 }
